996cf9bfcbf733a920b09d2a7113573c12aaf93f
felipefll <felipeferreiralopeslima@gmail.com>
2023-04-18 09:41:49 -0300

    Authenticated User Links

--- src/App.js
+++ src/App.js
@@ -14,6 +14,8 @@ import Login from "./pages/Login/Login";
 import Register from "./pages/Register/Register";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import CreatePost from "./pages/CreatePost/CreatePost";
+import Dashboard from "./pages/Dashboard/Dashboard";
 
 function App() {
   const [user, setUser] = useState(undefined);
@@ -42,6 +44,8 @@ function App() {
               <Route path="/about" element={<About />} />
               <Route path="/login" element={<Login />} />
               <Route path="/register" element={<Register />} />
+              <Route path="/post/create" element={<CreatePost />} />
+              <Route path="/dashboard" element={<Dashboard />} />
             </Routes>
           </div>
           <Footer />
