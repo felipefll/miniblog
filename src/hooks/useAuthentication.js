90d9b4000dad3e41fbb08ae5097d3d058f587698
felipefll <felipeferreiralopeslima@gmail.com>
2023-04-18 11:21:49 -0300

    user login

--- src/hooks/useAuthentication.js
+++ src/hooks/useAuthentication.js
@@ -3,7 +3,7 @@ import { db } from "../firebase/config";
 import {
   getAuth,
   createUserWithEmailAndPassword,
-  singInWithEmailAndPassword,
+  signInWithEmailAndPassword,
   updateProfile,
   signOut,
 } from "firebase/auth";
@@ -72,6 +72,31 @@ export const useAuthentication = () => {
     signOut(auth);
   };
 
+  //login
+  const login = async (data) => {
+    checkIfIsCancelled();
+    setLoading(true);
+    setError(false);
+
+    try {
+      await signInWithEmailAndPassword(auth, data.email, data.password);
+      setLoading(false);
+    } catch (error) {
+      let systemErrorMessage;
+
+      if (error.message.includes("user-not-found")) {
+        systemErrorMessage = "Usuário não encontrado";
+      } else if (error.message.includes("wrong-password")) {
+        systemErrorMessage = "Senha incorreta";
+      } else {
+        systemErrorMessage = "Ocorreu um erro, por favor tente mais tarde.";
+      }
+
+      setError(systemErrorMessage);
+      setLoading(false);
+    }
+  };
+
   useEffect(() => {
     return () => setCancelled(true);
   }, []);
@@ -82,5 +107,6 @@ export const useAuthentication = () => {
     error,
     loading,
     logout,
+    login,
   };
 };
